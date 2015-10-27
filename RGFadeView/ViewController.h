9d8d008473146b002340e6d936198699d41a6af2
arvin <rorogestudio@gmail.com>
2015-10-27 17:21:28 +0800

    Initial Commit

---
+++ RGFadeView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RGFadeView
+//
+//  Created by Arvin on 15/10/27.
+//  Copyright © 2015年 com.roroge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RGFadeView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RGFadeView
+//
+//  Created by Arvin on 15/10/27.
+//  Copyright © 2015年 com.roroge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
